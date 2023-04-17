4b1f96827a9f17ab385ee3c1bed168b1cd8de9db
nossbank <117912315+nossbank@users.noreply.github.com>
2023-04-17 19:11:20 -0300

    added king movements

--- src/main/java/br/com/yvestaba/xadrez/domain/pieces/King.java
+++ src/main/java/br/com/yvestaba/xadrez/domain/pieces/King.java
@@ -4,7 +4,11 @@ import br.com.yvestaba.xadrez.domain.Board;
 import br.com.yvestaba.xadrez.domain.Color;
 import br.com.yvestaba.xadrez.domain.Position;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
+
+import static br.com.yvestaba.xadrez.utils.ChessCommonUtils.addIfDoesNotExistOnBoardOrCapturable;
 
 public class King extends Piece{
 
@@ -13,6 +17,38 @@ public class King extends Piece{
     }
     @Override
     protected Set<Position> getPositions(Board board, Position position) {
-        return null;
+        var ret = new HashSet<Position>();
+        Integer col = position.getCol();
+        Integer lin = position.getLin();
+        Integer colPlusOne = col + 1;
+        Integer colMinusOne = col - 1;
+        Integer linPlusOne = lin + 1;
+        Integer linMinusOne = lin - 1;
+
+        var columns = new ArrayList<Integer>();
+        if(colPlusOne < 8){
+            columns.add(colPlusOne);
+            addIfDoesNotExistOnBoardOrCapturable(ret, board, new Position(colPlusOne, lin));
+        }
+        if(colMinusOne >= 0){
+            columns.add(colMinusOne);
+            addIfDoesNotExistOnBoardOrCapturable(ret, board, new Position(colMinusOne, lin));
+        }
+
+        var lines = new ArrayList<Integer>();
+        if(linPlusOne < 8){
+            lines.add(linPlusOne);
+            addIfDoesNotExistOnBoardOrCapturable(ret, board, new Position(col, linPlusOne));
+        }
+        if(linMinusOne >= 0){
+            lines.add(linMinusOne);
+            addIfDoesNotExistOnBoardOrCapturable(ret, board, new Position(col, linMinusOne));
+        }
+        for(var column : columns){
+            for(var line : lines){
+                addIfDoesNotExistOnBoardOrCapturable(ret, board, new Position(column, line));
+            }
+        }
+        return ret;
     }
 }
