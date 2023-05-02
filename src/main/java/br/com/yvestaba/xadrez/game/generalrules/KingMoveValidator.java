72c79ff9ffd4a477115e051ab9fc90086f96a1cf
Yves Taba <117912315+nossbank@users.noreply.github.com>
2023-05-02 14:00:13 -0300

    bugfix king movement

--- src/main/java/br/com/yvestaba/xadrez/game/generalrules/KingMoveValidator.java
+++ src/main/java/br/com/yvestaba/xadrez/game/generalrules/KingMoveValidator.java
@@ -1,19 +1,60 @@
 package br.com.yvestaba.xadrez.game.generalrules;
 
+import br.com.yvestaba.xadrez.game.Direction;
 import br.com.yvestaba.xadrez.game.GameStatus;
 import br.com.yvestaba.xadrez.game.Position;
 import br.com.yvestaba.xadrez.game.directionchecker.*;
+import br.com.yvestaba.xadrez.utils.ChessCommonUtils;
 
+import java.util.List;
 import java.util.Set;
 
 class KingMoveValidator {
 
     static Set<Position> validate(Position kingPosition, Set<Position> valid, ThreatChecker threatChecker, Board board){
         valid.removeAll(threatChecker.getThreatenArea());
-        if(board.getStatus() == GameStatus.CHECK){
+        if(threatChecker.getThreatenArea().contains(kingPosition)){
             DirectionChecker checker = getThreatDirection(kingPosition, board);
+            invalidateByDirections(valid, checker.getDirections(), kingPosition);
         }
         return valid;
+    }
+
+    private static void invalidateByDirections(Set<Position> valid, List<Direction> directions, Position kingPosition) {
+        for(var direction : directions){
+            switch(direction){
+                case N:
+                    validateAndRemove(valid, kingPosition.getCol(), kingPosition.getLin() - 1);
+                    break;
+                case S:
+                    validateAndRemove(valid, kingPosition.getCol(), kingPosition.getLin() + 1);
+                    break;
+                case E:
+                    validateAndRemove(valid, kingPosition.getCol() - 1, kingPosition.getLin());
+                    break;
+                case W:
+                    validateAndRemove(valid, kingPosition.getCol() + 1, kingPosition.getLin());
+                    break;
+                case NE:
+                    validateAndRemove(valid, kingPosition.getCol() - 1, kingPosition.getLin() - 1);
+                    break;
+                case NW:
+                    validateAndRemove(valid, kingPosition.getCol() + 1, kingPosition.getLin() - 1);
+                    break;
+                case SE:
+                    validateAndRemove(valid, kingPosition.getCol() + 1, kingPosition.getLin() + 1);
+                    break;
+                case SW:
+                    validateAndRemove(valid, kingPosition.getCol() - 1, kingPosition.getLin() + 1);
+                    break;
+            }
+        }
+    }
+
+    private static void validateAndRemove(Set<Position> set, int col, int lin){
+        if(ChessCommonUtils.validateColLin(col, lin)){
+            set.remove(new Position(col, lin));
+        }
     }
 
     private static DirectionChecker getThreatDirection(Position positionKing, Board board) {
--- src/test/java/br/com/yvestaba/xadrez/game/generalrules/PiecesMoverTest.java
+++ src/test/java/br/com/yvestaba/xadrez/game/generalrules/PiecesMoverTest.java
@@ -31,5 +31,17 @@ class PiecesMoverTest {
         PlayReader.play("1.e4 d5 2.exd5 Qxd5 3.Nc3 Qd8 4.Bc4 Nf6 5.Nf3 Bg4 6.h3 Bxf3 7.Qxf3 e6 8.Qxb7 Nbd7 9.Nb5 Rc8 10.Nxa7 Nb6 11.Nxc8 Nxc8 12.d4 Nd6 13.Bb5+ Nxb5 14.Qxb5+ Nd7 15.d5 exd5 16.Be3 Bd6 17.Rd1 Qf6 18.Rxd5 Qg6 19.Bf4 Bxf4 20.Qxd7+ Kf8 21.Qd8#");
     }
 
+    //https://www.chessgames.com/perl/chessgame?gid=1581433
+    @Test
+    void pedroDamianoVsNN(){
+        PlayReader.play("1.e4 e5 2.Nf3 f6 3.Nxe5 fxe5 4.Qh5+ Ke7 5.Qxe5+ Kf7 6.Bc4+ d5 7.Bxd5+ Kg6 8.f4 h6 9.f5+ Kh7 10.Bf7 Ne7 11.Qg3 Nbc6 12.Qg6+ Nxg6 13.fxg6#");
+    }
+
+    //https://www.chessgames.com/perl/chessgame?gid=2055543
+    @Test
+    void alexeiVsVincent(){
+        PlayReader.play("1.c4 e6 2.Nc3 d5 3.cxd5 exd5 4.d4 c6 5.Bf4 Bd6 6.Bg3 Nf6 7.e3 Bf5 8.Bd3 Bxd3 9.Qxd3 O-O 10.Nge2 Re8 11.O-O Nh5 12.Bxd6 Qxd6 13.Rab1 a5 14.Ng3 Nxg3 15.hxg3 Nd7 16.Ne2 Nf6 17.Qa3 Qd7 18.f3 h5 19.Rbe1 Qf5 20.Nf4 g6 21.Qd3 Qg5 22.Kf2 Nh7 23.Rh1 Re7 24.a4 Qf6 25.Rh4 Ng5 26.Reh1 Rae8 27.Re1 Ne6 28.Nxe6 Rxe6 29.Re2 Qe7 30.Rh1 Qb4 31.b3 Qa3 32.Qc3 b6 33.Rhe1 Qd6 34.e4 dxe4 35.Rxe4 h4 36.gxh4 Rxe4 37.Rxe4 Rxe4 38.fxe4 Qf4+ 39.Kg1 Qxe4 40.g3 Kf8 41.Kf2 Qd5 42.g4 Qd6 43.Kg2 Qf6 44.g5 Qe6 45.Kg3 Ke8 46.Qf3 Qe1+ 47.Kh3 Qe6+ 48.Kg2 Kd7 49.Kf2 Kd6 50.Qg3+ Kd5");
+    }
+
 
 }
